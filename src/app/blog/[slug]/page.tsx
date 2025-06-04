96e70989246dbdda2dbd5d1b1c8a88e45d62b4f8
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-06-03 20:49:10 -0600

    Blog list page and individual page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,103 @@
+// File: /app/[slug]/page.tsx
+
+import { PortableText } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { client } from "@/sanity/client";
+import Link from "next/link";
+import Image from "next/image";
+
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  body,
+  publishedAt,
+  "imageUrl": mainImage.asset->url,
+  event->{
+    _id,
+    name,
+    slug,
+    "coverImageUrl": coverImage.asset->url,
+    description,
+  },
+  tags[]->{
+    _id,
+    title
+  }
+}`;
+
+const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
+
+const options = { next: { revalidate: 30 } };
+
+interface PostPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const resolvedParams = await params;
+  const post = await client.fetch(
+    POST_QUERY,
+    { slug: resolvedParams.slug },
+    options
+  );
+
+  if (!resolvedParams?.slug) {
+    return <div>Error: No slug provided.</div>;
+  }
+
+  if (!post) {
+    return (
+      <main className="container mx-auto min-h-screen max-w-3xl p-8">
+        <p className="text-red-500">Post not found.</p>
+        <Link href="/" className="text-blue-500 hover:underline">
+          ← Back to posts
+        </Link>
+      </main>
+    );
+  }
+
+  const postImageUrl = post.event.coverImageUrl
+    ? urlFor(post.event.coverImageUrl)?.width(550).height(550).url()
+    : null;
+
+  console.log("Fetched post:", post);
+
+  return (
+    <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
+      <Link href="/" className="hover:underline">
+        ← Back to posts
+      </Link>
+
+      {postImageUrl && (
+        <Image
+          src={postImageUrl}
+          alt={post.title}
+          className="mt-2 w-full h-auto rounded-lg"
+          width={800}
+          height={600}
+        />
+      )}
+
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <div className="prose">
+        <p className="text-1xl font-bold mb-4">
+          Published: {new Date(post.publishedAt).toLocaleDateString()}
+        </p>
+        {Array.isArray(post.body) && (
+          <div className="pt-4 space-y-4">
+            <PortableText value={post.body} />
+          </div>
+        )}
+      </div>
+      <h2>{post.endDate}</h2>
+    </main>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -26,18 +26,18 @@ const POSTS_QUERY = `*[_type == "post"] | order(publishedAt desc)[0...10] {
 
 const options = { next: { revalidate: 30 } };
 
-export default async function IndexPage() {
+export default async function BlogPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   console.log("Fetched posts:", posts);
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Foundation Collective</h1>
+      <h1 className="text-4xl font-bold mb-8">Foundation Blog</h1>
       <ul className="flex flex-col gap-y-4">
         {posts.map((post) => (
           <li className="hover:underline" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
+            <Link href={`/blog/${post.slug.current}`}>
               <h2 className="text-xl font-semibold">{post.title}</h2>
               <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
               <p className="text-gray-600">{post.description}</p>
