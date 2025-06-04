1754ae8e493567dc82ecebb4c90fb31eb6a3c248
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2025-06-03 20:57:37 -0600

    Event Page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -7,24 +7,14 @@ import { client } from "@/sanity/client";
 import Link from "next/link";
 import Image from "next/image";
 
-const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]{
+const EVENT_QUERY = `*[_type == "event" && slug.current == $slug][0]{
   _id,
-  title,
+  name,
   slug,
-  body,
-  publishedAt,
-  "imageUrl": mainImage.asset->url,
-  event->{
-    _id,
-    name,
-    slug,
-    "coverImageUrl": coverImage.asset->url,
-    description,
-  },
-  tags[]->{
-    _id,
-    title
-  }
+  "coverImageUrl": coverImage.asset->url,
+  description,
+  startDate,
+  endDate,
 }`;
 
 const { projectId, dataset } = client.config();
@@ -43,8 +33,8 @@ interface PostPageProps {
 
 export default async function PostPage({ params }: PostPageProps) {
   const resolvedParams = await params;
-  const post = await client.fetch(
-    POST_QUERY,
+  const event = await client.fetch(
+    EVENT_QUERY,
     { slug: resolvedParams.slug },
     options
   );
@@ -53,7 +43,7 @@ export default async function PostPage({ params }: PostPageProps) {
     return <div>Error: No slug provided.</div>;
   }
 
-  if (!post) {
+  if (!event) {
     return (
       <main className="container mx-auto min-h-screen max-w-3xl p-8">
         <p className="text-red-500">Post not found.</p>
@@ -64,40 +54,40 @@ export default async function PostPage({ params }: PostPageProps) {
     );
   }
 
-  const postImageUrl = post.event.coverImageUrl
-    ? urlFor(post.event.coverImageUrl)?.width(550).height(550).url()
+  const eventImageUrl = event.coverImageUrl
+    ? urlFor(event.coverImageUrl)?.width(550).height(550).url()
     : null;
-
-  console.log("Fetched post:", post);
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
       <Link href="/" className="hover:underline">
-        ← Back to posts
+        ← Back to home
       </Link>
 
-      {postImageUrl && (
+      {eventImageUrl && (
         <Image
-          src={postImageUrl}
-          alt={post.title}
+          src={eventImageUrl}
+          alt={event.name}
           className="mt-2 w-full h-auto rounded-lg"
           width={800}
           height={600}
         />
       )}
 
-      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <h1 className="text-4xl font-bold mb-4">{event.title}</h1>
       <div className="prose">
         <p className="text-1xl font-bold mb-4">
-          Published: {new Date(post.publishedAt).toLocaleDateString()}
+          Starts: {new Date(event.startDate).toLocaleDateString()}
         </p>
-        {Array.isArray(post.body) && (
+        <p className="text-1xl font-bold mb-4">
+          Ends: {new Date(event.endDate).toLocaleDateString()}
+        </p>
+        {Array.isArray(event.description) && (
           <div className="pt-4 space-y-4">
-            <PortableText value={post.body} />
+            <PortableText value={event.description} />
           </div>
         )}
       </div>
-      <h2>{post.endDate}</h2>
     </main>
   );
 }
