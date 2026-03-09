5c8b72ac39bc68de1a1d3e16e0e474398f0c999a
Amanda Haynes <6064242+amandala@users.noreply.github.com>
2026-03-08 18:11:06 -0600

    Revert "Prevent iOS page zoom with viewport maximum-scale"
    This reverts commit 57d1bb095c523f342b01c684885a750a52cc90fd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,12 +26,6 @@ export const metadata = {
   description: "Foundation Collective",
 };
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-};
-
 export default async function RootLayout({
   children,
 }: Readonly<{
